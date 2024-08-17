be30b73863095cca37cab6ed76ca5f0672891bea
SEUNSAMSON94 <121940090+SEUNSAMSON94@users.noreply.github.com>
2024-08-17 21:06:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express =  require('express')
+const app = express();
+const exhbs = require("express-handlebars")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(2323, ()=>{
+    console.log('server is running on port 2323')
+})
